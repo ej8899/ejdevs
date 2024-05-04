dce937db3d34344ee8c455176d830458ab3089df
ej8899 <ej8899@gmail.com>
2024-05-04 19:19:59 -0400

    update: tweak for sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Button, Navbar, DarkThemeToggle } from "flowbite-react";
 
 export default function Component() {
   return (
-    <Navbar fluid rounded className="w-full">
+    <Navbar fluid rounded className="w-full sticky top-0 z-50">
       <Navbar.Brand href="https://flowbite-react.com">
         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="EJDevs Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">EJDevs.com</span>
