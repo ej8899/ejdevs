958d2452965626c61a23264dd7fe6163dd93799f
ej8899 <ej8899@gmail.com>
2024-05-04 16:22:22 -0400

    add: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+
+"use client";
+
+import { Button, Navbar, DarkThemeToggle } from "flowbite-react";
+
+export default function Component() {
+  return (
+    <Navbar fluid rounded className="w-full">
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="EJDevs Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">EJDevs.com</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        {/* <Button>Get started</Button> */}
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+        
+      </Navbar.Collapse>
+      <DarkThemeToggle />
+    </Navbar>
+  );
+}
