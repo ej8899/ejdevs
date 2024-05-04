cf1be24ba969e323ca397f1623486cdb45b7f1f0
ej8899 <ej8899@gmail.com>
2024-05-04 19:19:43 -0400

    add: newsletter section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import OurNavbar from "./components/Navbar";
 import OurFooter from "./components/Footer";
 import Hero from "./components/Hero";
 import ContactSection from "./components/Contact";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
     <main className="flex min-h-screen w-full flex-col justify-center gap-2 dark:bg-gray-800">
       <OurNavbar/>
       <Hero/>
+      <Newsletter/>
       <ContactSection/>
       <OurFooter/>
     </main>
