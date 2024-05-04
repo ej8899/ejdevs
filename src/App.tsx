d2772595a93b770cc74aa901c8e09ac0ae3c36a9
ej8899 <ej8899@gmail.com>
2024-05-04 16:22:05 -0400

    update: adjust outline, add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { DarkThemeToggle } from "flowbite-react";
+import { DarkThemeToggle, } from "flowbite-react";
+
+import OurNavbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
-      <DarkThemeToggle />
+    <main className="flex min-h-screen w-full justify-center gap-2 dark:bg-gray-800">
+      <OurNavbar/>
+      
     </main>
   );
 }
