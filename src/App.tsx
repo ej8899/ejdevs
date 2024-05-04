ad97479654136554584bead1e9dbe49c8e502e1f
ej8899 <ej8899@gmail.com>
2024-05-04 18:23:15 -0400

    add: additional sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { DarkThemeToggle, } from "flowbite-react";
 
 import OurNavbar from "./components/Navbar";
+import OurFooter from "./components/Footer";
+import Hero from "./components/Hero";
+import ContactSection from "./components/Contact";
 
 function App() {
   return (
-    <main className="flex min-h-screen w-full justify-center gap-2 dark:bg-gray-800">
+    <main className="flex min-h-screen w-full flex-col justify-center gap-2 dark:bg-gray-800">
       <OurNavbar/>
-      
+      <Hero/>
+      <ContactSection/>
+      <OurFooter/>
     </main>
   );
 }
